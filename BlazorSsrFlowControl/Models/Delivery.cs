845405321ea992931bc2a8d06dc31a38936570a9
andriyskb <148741323+andriyskb@users.noreply.github.com>
2023-12-19 18:06:01 +0000

    avanzando

--- BlazorSsrFlowControl/Models/Delivery.cs
+++ BlazorSsrFlowControl/Models/Delivery.cs
@@ -6,7 +6,6 @@
         public int OrderId { get; set; }
         public int Hour { get; set; }
         public int Minute { get; set; }
-        public int Day { get; set; }
-        public int Month { get; set; }
+        public int Quantity { get; set; }
     }
 }
