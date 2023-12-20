3e15618b92908257aeefb8cb48298cac5f8356d5
andriyskb <148741323+andriyskb@users.noreply.github.com>
2023-12-20 14:35:57 +0000

    avanzando

--- BlazorSsrFlowControl/Models/Delivery.cs
+++ BlazorSsrFlowControl/Models/Delivery.cs
@@ -3,9 +3,9 @@
     public class Delivery
     {
         public int Id { get; set; }
-        public int OrderId { get; set; }
-        public int Hour { get; set; }
-        public int Minute { get; set; }
-        public int Quantity { get; set; }
+        public int? OrderId { get; set; }
+        public int? Hour { get; set; }
+        public int? Minute { get; set; }
+        public int? Quantity { get; set; }
     }
 }
