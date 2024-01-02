5d17743204e131505a9cde531f7518010df6c2b6
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2024-01-02 19:01:36 -0300

    configuring cors

---
+++ BlazorSsrFlowControl/Utils/FormatNumbers.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorSsrFlowControl.Utils
+{
+    public class FormatNumbers
+    {
+        public static string? Time(int? n)
+        {
+            return n?.ToString().Length == 1 ? "0" + n : n.ToString();
+        }
+    }
+}
